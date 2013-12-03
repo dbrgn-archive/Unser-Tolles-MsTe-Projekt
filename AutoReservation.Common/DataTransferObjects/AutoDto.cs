88ca0a052e75372289a76c3cd4db171057e555af
Jonas <dizzgo@gmail.com>
2013-12-03 18:26:36 +0100

    dtos erstellt

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -4,52 +4,111 @@ using System.Text;
 namespace AutoReservation.Common.DataTransferObjects
 {
     [DataContract]
-    public class AutoDto //: DtoBase
+    public class AutoDto : DtoBase
     {
+        private int id;
+        private string marke;
+        private int tagestarif;
+        private int basistarif;
+        private AutoKlasse autoKlasse;
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(marke))
-        //    {
-        //        error.AppendLine("- Marke ist nicht gesetzt.");
-        //    }
-        //    if (tagestarif <= 0)
-        //    {
-        //        error.AppendLine("- Tagestarif muss grösser als 0 sein.");
-        //    }
-        //    if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
-        //    {
-        //        error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
-        //    }
+        public int Id {
+            get { return id; }
+            set
+            {
+                if (id != value) {
+                    SendPropertyChanging(() => Id);
+                    id = value;
+                    SendPropertyChanged(() => Id);
+                }
+            }
+        }
+        public string Marke {
+            get { return marke; } 
+            set {
+                if (marke != value) 
+                {
+                    SendPropertyChanging(() => Marke);
+                    marke = value;
+                    SendPropertyChanged(()=> Marke);
+                }
+            } 
+        }
+        public int Tagestarif  {
+            get { return tagestarif; }
+            set
+            {
+                if (tagestarif!= value) {
+                    SendPropertyChanging(() => Tagestarif);
+                    tagestarif = value;
+                    SendPropertyChanged(() => Tagestarif);
+                }
+            }
+        }
+        public int Basistarif
+        {
+            get { return basistarif; }
+            set
+            {
+                if (basistarif != value) {
+                    SendPropertyChanging(() => Basistarif);
+                    basistarif = value;
+                    SendPropertyChanged(() => Basistarif);
+                }
+            }
+        }
+        public AutoKlasse AutoKlasse
+        {
+            get { return autoKlasse; }
+            set
+            {
+                if (autoKlasse != value) {
+                    SendPropertyChanging(() => AutoKlasse);
+                    autoKlasse = value;
+                    SendPropertyChanged(() => AutoKlasse);
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(marke)) {
+                error.AppendLine("- Marke ist nicht gesetzt.");
+            }
+            if (tagestarif <= 0) {
+                error.AppendLine("- Tagestarif muss grösser als 0 sein.");
+            }
+            if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0) {
+                error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
+            }
 
-        //    return error.ToString();
-        //}
+            if (error.Length == 0) { return null; }
 
-        //public override object Clone()
-        //{
-        //    return new AutoDto
-        //    {
-        //        Id = Id,
-        //        Marke = Marke,
-        //        Tagestarif = Tagestarif,
-        //        AutoKlasse = AutoKlasse,
-        //        Basistarif = Basistarif
-        //    };
-        //}
+            return error.ToString();
+        }
 
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}; {4}",
-        //        Id,
-        //        Marke,
-        //        Tagestarif,
-        //        Basistarif,
-        //        AutoKlasse);
-        //}
+        public override object Clone()
+        {
+            return new AutoDto {
+                Id = Id,
+                Marke = Marke,
+                Tagestarif = Tagestarif,
+                AutoKlasse = AutoKlasse,
+                Basistarif = Basistarif
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}; {4}",
+                Id,
+                Marke,
+                Tagestarif,
+                Basistarif,
+                AutoKlasse);
+        }
 
     }
 }
--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -5,50 +5,103 @@ using System.Text;
 namespace AutoReservation.Common.DataTransferObjects
 {
     [DataContract]
-    public class KundeDto //: DtoBase
+    public class KundeDto : DtoBase
     {
+        private string nachname;
+        private string vorname;
+        private int id;
+        private DateTime geburtsdatum;
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(Nachname))
-        //    {
-        //        error.AppendLine("- Nachname ist nicht gesetzt.");
-        //    }
-        //    if (string.IsNullOrEmpty(Vorname))
-        //    {
-        //        error.AppendLine("- Vorname ist nicht gesetzt.");
-        //    }
-        //    if (Geburtsdatum == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
-        //    }
+        public string Nachname
+        {
+            get { return nachname; }
+            set
+            {
+                if (nachname != value) {
+                    SendPropertyChanging(() => Nachname);
+                    nachname = value;
+                    SendPropertyChanged(() => Nachname);
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        public string Vorname
+        {
+            get { return vorname; }
+            set
+            {
+                if (vorname != value) {
+                    SendPropertyChanging(() => Vorname);
+                    vorname = value;
+                    SendPropertyChanged(() => Vorname);
+                }
+            }
+        }
 
-        //    return error.ToString();
-        //}
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value) {
+                    SendPropertyChanging(() => Id);
+                    id = value;
+                    SendPropertyChanged(() => Id);
+                }
+            }
+        }
 
-        //public override object Clone()
-        //{
-        //    return new KundeDto
-        //    {
-        //        Id = Id,
-        //        Nachname = Nachname,
-        //        Vorname = Vorname,
-        //        Geburtsdatum = Geburtsdatum
-        //    };
-        //}
+        public DateTime Geburtsdatum
+        {
+            get { return geburtsdatum; }
+            set
+            {
+                if (geburtsdatum != value) {
+                    SendPropertyChanging(() => Geburtsdatum);
+                    geburtsdatum = value;
+                    SendPropertyChanged(() => Geburtsdatum);
+                }
+            }
+        }
 
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}",
-        //        Id,
-        //        Nachname,
-        //        Vorname,
-        //        Geburtsdatum);
-        //}
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(Nachname)) {
+                error.AppendLine("- Nachname ist nicht gesetzt.");
+            }
+            if (string.IsNullOrEmpty(Vorname)) {
+                error.AppendLine("- Vorname ist nicht gesetzt.");
+            }
+            if (Geburtsdatum == DateTime.MinValue) {
+                error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override object Clone()
+        {
+            return new KundeDto {
+                Id = Id,
+                Nachname = Nachname,
+                Vorname = Vorname,
+                Geburtsdatum = Geburtsdatum
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}",
+                Id,
+                Nachname,
+                Vorname,
+                Geburtsdatum);
+        }
 
     }
 }
