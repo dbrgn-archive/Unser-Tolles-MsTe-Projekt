719d012335bbce12272ff774de2974ef88d4a01f
Jonas <dizzgo@gmail.com>
2013-12-04 19:52:17 +0100

    Finished Service Interface and Service, also moved files so the testproject will run.

---
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -0,0 +1,55 @@
+﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace AutoReservation.Common.Interfaces
+{
+	[ServiceContract(Namespace = "http://localhost:7876/AutoReservationService")]
+	public interface IAutoReservationService
+	{
+		[OperationContract]
+		List<AutoDto> GetAutos();
+
+		[OperationContract]
+		void AddAuto(AutoDto auto);
+
+		[OperationContract]
+		void RemoveAuto(AutoDto auto);
+
+		[OperationContract]
+		void UpdateAuto(AutoDto modified, AutoDto original);
+
+		[OperationContract]
+		AutoDto GetAutoById(int id);
+
+		[OperationContract]
+		List<KundeDto> GetKunden();
+
+		[OperationContract]
+		void AddKunde(KundeDto kunde);
+
+		[OperationContract]
+		void RemoveKunde(KundeDto kunde);
+
+		[OperationContract]
+		void UpdateKunde(KundeDto modified, KundeDto original);
+
+		[OperationContract]
+		KundeDto GetKundeById(int id);
+
+		[OperationContract]
+		List<ReservationDto> GetReservationen();
+
+		[OperationContract]
+		void AddReservation(ReservationDto reservation);
+
+		[OperationContract]
+		void RemoveReservation(ReservationDto reservation);
+
+		[OperationContract]
+		void UpdateReservation(ReservationDto modified, ReservationDto original);
+
+		[OperationContract]
+		ReservationDto GetReservationByNr(int resNr);
+	}
+}
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,13 +1,118 @@
-﻿using System;
+﻿using AutoReservation.BusinessLayer;
+using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
-    {
-        private static void WriteActualMethod()
-        {
-            Console.WriteLine("Calling: " + new StackTrace().GetFrame(1).GetMethod().Name);
-        }
-    }
+	public class AutoReservationService : IAutoReservationService
+	{
+		private AutoReservationBusinessComponent businessComponent;
+
+		private AutoReservationBusinessComponent getBusinessComponent()
+		{
+			if (businessComponent == null)
+			{
+				businessComponent = new AutoReservationBusinessComponent();
+			}
+			return businessComponent;
+		}
+
+		private static void WriteActualMethod()
+		{
+			Console.WriteLine("Calling: " + new StackTrace().GetFrame(1).GetMethod().Name);
+		}
+
+		public List<AutoDto> GetAutos()
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetAutos().ConvertToDtos();
+		}
+
+		public void AddAuto(AutoDto auto)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().AddAuto(auto.ConvertToEntity());
+		}
+
+		public void RemoveAuto(AutoDto auto)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().RemoveAuto(auto.ConvertToEntity());
+		}
+
+		public void UpdateAuto(AutoDto modified, AutoDto original)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().UpdateAuto(modified.ConvertToEntity(), original.ConvertToEntity());
+		}
+
+		public AutoDto GetAutoById(int id)
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetAutoById(id).ConvertToDto();
+		}
+
+		public List<KundeDto> GetKunden()
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetKunden().ConvertToDtos();
+		}
+
+		public void AddKunde(KundeDto kunde)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().AddKunde(kunde.ConvertToEntity());
+		}
+
+		public void RemoveKunde(KundeDto kunde)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().RemoveKunde(kunde.ConvertToEntity());
+		}
+
+		public void UpdateKunde(KundeDto modified, KundeDto original)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().UpdateKunde(modified.ConvertToEntity(), original.ConvertToEntity());
+		}
+
+		public KundeDto GetKundeById(int id)
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetKundeById(id).ConvertToDto();
+		}
+
+		public List<ReservationDto> GetReservationen()
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetReservationen().ConvertToDtos();
+		}
+
+		public void AddReservation(ReservationDto reservation)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().AddReservation(reservation.ConvertToEntity());
+		}
+
+		public void RemoveReservation(ReservationDto reservation)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().RemoveReservation(reservation.ConvertToEntity());
+		}
+
+		public void UpdateReservation(ReservationDto modified, ReservationDto original)
+		{
+			AutoReservationService.WriteActualMethod();
+			this.getBusinessComponent().UpdateReservation(modified.ConvertToEntity(), original.ConvertToEntity());
+		}
+
+		public ReservationDto GetReservationByNr(int resNr)
+		{
+			AutoReservationService.WriteActualMethod();
+			return this.getBusinessComponent().GetReservationByNr(resNr).ConvertToDto();
+		}
+	}
 }
