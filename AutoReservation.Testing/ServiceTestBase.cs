2c3fc3dbf30a306111612d364543e8793f3d33eb
Jonas <dizzgo@gmail.com>
2013-12-08 11:44:15 +0100

    Added more tests

--- AutoReservation.Testing/ServiceTestBase.cs
+++ AutoReservation.Testing/ServiceTestBase.cs
@@ -5,6 +5,8 @@ using AutoReservation.Common.DataTransferObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AutoReservation.Common.Interfaces;
 using AutoReservation.BusinessLayer.Testing;
+using AutoReservation.Dal;
+using AutoReservation.BusinessLayer;
 
 namespace AutoReservation.Testing
 {
@@ -77,17 +79,15 @@ namespace AutoReservation.Testing
 		[TestMethod]
 		public void InsertAutoTest()
 		{
-			const int id = 4;
 			const string marke = "Volvo";
 			const int tarif = 100;
 			AutoDto newAuto = new AutoDto();
-			newAuto.Id = id;
 			newAuto.Marke = marke;
 			newAuto.Tagestarif = tarif;
 			newAuto.AutoKlasse = AutoKlasse.Mittelklasse;
 			Target.AddAuto(newAuto);
 
-			AutoDto newAutoRet = Target.GetAutoById(id);
+			AutoDto newAutoRet = Target.GetAutoById(4);
 			Assert.AreEqual(marke, newAutoRet.Marke);
 			Assert.AreEqual(tarif, newAutoRet.Tagestarif);
 			Assert.AreEqual(AutoKlasse.Mittelklasse, newAutoRet.AutoKlasse);
@@ -96,43 +96,135 @@ namespace AutoReservation.Testing
 		[TestMethod]
 		public void InsertKundeTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			const string vorname = "Dr.";
+			const string nachname = "Who";
+			DateTime geburtsdatum = DateTime.Now;
+			KundeDto newKunde = new KundeDto();
+			newKunde.Vorname = vorname;
+			newKunde.Nachname = nachname;
+			newKunde.Geburtsdatum = geburtsdatum;
+			Target.AddKunde(newKunde);
+
+			KundeDto newKundeRet = Target.GetKundeById(5);
+			Assert.AreEqual(vorname, newKundeRet.Vorname);
+			Assert.AreEqual(nachname, newKundeRet.Nachname);
+			Assert.AreEqual(geburtsdatum.ToShortDateString(), newKundeRet.Geburtsdatum.ToShortDateString());
+
 		}
 
 		[TestMethod]
 		public void InsertReservationTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			DateTime von = DateTime.Now;
+			DateTime bis = DateTime.Now;
+			ReservationDto newRes = new ReservationDto();
+			AutoDto fiat = Target.GetAutoById(1);
+			KundeDto chrigi = Target.GetKundeById(1);
+			newRes.Auto = fiat;
+			newRes.Kunde = chrigi;
+			newRes.Von = von;
+			newRes.Bis = bis;
+			Target.AddReservation(newRes);
+
+			ReservationDto newResRet = Target.GetReservationByNr(4);
+			Assert.AreEqual(fiat.Marke, newResRet.Auto.Marke);
+			Assert.AreEqual(chrigi.Vorname, newResRet.Kunde.Vorname);
+			Assert.AreEqual(von.ToShortDateString(), newResRet.Von.ToShortDateString());
+			Assert.AreEqual(bis.ToShortDateString(), newResRet.Bis.ToShortDateString());
+
 		}
 
 		[TestMethod]
 		public void UpdateAutoTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			const string marke = "Volvo";
+			AutoDto newAuto = Target.GetAutoById(1);
+			newAuto.Marke = marke;
+			AutoDto origAuto = Target.GetAutoById(1);
+			Target.UpdateAuto(newAuto, origAuto);
+
+			AutoDto newAutoRet = Target.GetAutoById(1);
+			Assert.AreEqual(marke, newAutoRet.Marke);
+
 		}
 
 		[TestMethod]
 		public void UpdateKundeTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			const string vorname = "Elchbert";
+			KundeDto newKunde = Target.GetKundeById(1);
+			newKunde.Vorname = vorname;
+			KundeDto origKunde = Target.GetKundeById(1);
+			Target.UpdateKunde(newKunde, origKunde);
+
+			KundeDto newKundeRet = Target.GetKundeById(1);
+			Assert.AreEqual(vorname, newKundeRet.Vorname);
 		}
 
 		[TestMethod]
 		public void UpdateReservationTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			AutoDto auto = Target.GetAutoById(3);
+			ReservationDto newRes = Target.GetReservationByNr(1);
+			newRes.Auto = auto;
+			ReservationDto origRes = Target.GetReservationByNr(1);
+			Target.UpdateReservation(newRes, origRes);
+
+			ReservationDto newResRet = Target.GetReservationByNr(1);
+			Assert.AreEqual(auto.Marke, newResRet.Auto.Marke);
 		}
 
 		[TestMethod]
 		public void UpdateAutoTestWithOptimisticConcurrency()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			const string marke = "Volvo";
+			const int tarif = 1000;
+			AutoDto newAuto1 = Target.GetAutoById(1);
+			newAuto1.Marke = marke;
+			AutoDto newAuto2 = Target.GetAutoById(1);
+			newAuto2.Tagestarif = tarif;
+			AutoDto origAuto = Target.GetAutoById(1);
+			Target.UpdateAuto(newAuto2, origAuto);
+
+			try
+			{
+				Target.UpdateAuto(newAuto1, origAuto);
+			}
+			catch (LocalOptimisticConcurrencyException<Auto>)
+			{
+				
+			}
+			catch
+			{
+				Assert.Fail("Wrong Concurrency Exception, Auto");
+			}
 		}
 
 		[TestMethod]
 		public void UpdateKundeTestWithOptimisticConcurrency()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			const string vorname = "Elchbert";
+			const string nachname = "Gubbler";
+			KundeDto newKunde1 = Target.GetKundeById(1);
+			newKunde1.Vorname = vorname;
+			KundeDto newKunde2 = Target.GetKundeById(1);
+			newKunde2.Nachname = nachname;
+			KundeDto origKunde = Target.GetKundeById(1);
+			Target.UpdateKunde(newKunde2, origKunde);
+
+			try
+			{
+				Target.UpdateKunde(newKunde1, origKunde);
+			}
+			catch (LocalOptimisticConcurrencyException<Kunde>)
+			{
+			
+			} 
+			catch
+			{
+				Assert.Fail("Wrong Concurrency Exception, Kunde");
+			}
+		
 		}
 
 		[TestMethod]
@@ -144,19 +236,25 @@ namespace AutoReservation.Testing
 		[TestMethod]
 		public void DeleteKundeTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			KundeDto kunde = Target.GetKundeById(1);
+			Target.RemoveKunde(kunde);
+			Assert.AreEqual(3, Target.GetKunden().Count);
 		}
 
 		[TestMethod]
 		public void DeleteAutoTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			AutoDto auto = Target.GetAutoById(1);
+			Target.RemoveAuto(auto);
+			Assert.AreEqual(2, Target.GetAutos().Count);	
 		}
 
 		[TestMethod]
 		public void DeleteReservationTest()
 		{
-			Assert.Inconclusive("Test wurde noch nicht implementiert!");
+			ReservationDto res = Target.GetReservationByNr(1);
+			Target.RemoveReservation(res);
+			Assert.AreEqual(2, Target.GetReservationen().Count);
 		}
 	}
 }
